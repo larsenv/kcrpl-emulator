e7a1afcc19155a8c2bf76448b0b05045bc1f3ed5
Larsen Vallecillo <larsen@Weborican.com>
2017-10-23 14:53:40 -0500

    Add files via upload

---
+++ kcrpl.py
@@ -0,0 +1,29 @@
+import random
+import time
+
+print("KcrPL Emulator\n")
+
+tasks = ["Have any ideas why this guy is having issues?", "I'm gonna go take a shower.", "I'm going to sleep. Good night!", "Can I make a Wii emulator in Windows Batch?", "Make a support@riiconnect24.net email.", "Send me the new IOS patches please.", "I want to implement translation support in my batch script!", "Should I merge this PR?", "Help me I'm still getting 221001.", "I wanna be admin!", "Can you ban this spammer?", "Can I add more cities for Poland?"]
+tag_notag = ["@Larsenv ", ""]
+
+print("KcrPL: " + random.choice(tag_notag) + random.choice(tasks))
+
+time.sleep(1)
+
+response = ["Yes", "No", "OK", "Could you stop bothering me?"]
+
+print("Larsenv: " + random.choice(response))
+
+time.sleep(1)
+
+response = ["👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "👌", "/kcrplban", "/kcrplbaseball", "/kcrplclap", "/kcrplclap2", "/kcrplcry", "/kcrpldance", "/kcrpldance2", "/kcrpldance3", "/kcrpldance4", "/kcrpldance5", "/kcrplevidence", "/kcrplfacepalm", "/kcrplfuckyou", "/kcrplhaha", "/kcrplhmm", "/kcrpllarsenv", "/kcrplno", "/kcrplno2", "/kcrplshame", "/kcrplyes", "/kcrplyes2", "/kcrplyes3"]
+
+tag = random.choice(response)
+
+print("KcrPL: " + tag)
+
+if "/" in tag:
+    time.sleep(1)
+    print("Spectra: <Destiny GIF>")
+
+time.sleep(1)
